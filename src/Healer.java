d94208dddc35cfa76a6fd70add8bb18029f277d3
941923 <941923@757-373-14-065.mcpsmd.org>
2014-05-12 14:09:11 -0400

    Healers buffed.

--- src/Healer.java
+++ src/Healer.java
@@ -5,14 +5,14 @@ public class Healer extends Adventurer
 	{
 		super(m,x,y);
 		tileType=HEALER;
-		baseAttack=-1;
-		currentAttack=-1;
+		baseAttack=-2;
+		currentAttack=-2;
 		attackRange=1;
 		abilities[REVIVE]=true;
 		baseHealth=10;
 		currentHealth=10;
 		moveRange=1;
-		regenRate=1;
+		regenRate=3;
 		visionRange=5;
 	}
 }
