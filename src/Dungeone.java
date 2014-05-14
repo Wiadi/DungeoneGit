577a1037b6050397b1c93b94a5db210ae336ee10
Llording <LlamaLlord27@gmail.com>
2014-05-14 00:55:44 -0400

    Adjusted Color Scheme Designed Loading Screen/Instructions

--- src/Dungeone.java
+++ src/Dungeone.java
@@ -46,7 +46,7 @@ public class Dungeone extends Canvas{
 	private BufferedImage buff;
 	private KeyEvent event;
 	private MouseEvent event2;
-	private final static int WIDTH=50;
+	private final static int WIDTH=40;
 	private final static int HEIGHT=40;
 	
 	//FOR NETWORKING
@@ -162,6 +162,7 @@ public class Dungeone extends Canvas{
 		if(turn == 1)
 			for(Monster m: mobs)
 				m.regen();
+		ranges = false;
 		update();
 		
 		cont = true;
@@ -515,17 +516,17 @@ public class Dungeone extends Canvas{
 //						map.placeTile(select[0], select[1], 2, inky);
 //						action[turn]-=5;
 //						break;
-					case 2:
+					case 1:
 						LivingWall wall = new LivingWall(map, select[0], select[1]);
 						mobs.add(wall);
 						map.placeTile(select[0], select[1], 2, wall);
 						action[turn]-=5;
 						break;
-					case 3:
+					case 2:
 						Bulk hulk = new Bulk(map, select[0], select[1]);
 						mobs.add(hulk);
 						map.placeTile(select[0], select[1], 2, hulk);
-						action[turn]-=5;
+						action[turn]-=10;
 						break;
 					}
 				}
@@ -578,8 +579,11 @@ public class Dungeone extends Canvas{
 			Graphics2D g2 = (Graphics2D) g;
 			g2.drawImage(buff, 0, 0, getWidth(), getHeight(), null);
 		}
-		else
+		else{
 			System.out.println("No Image");
+			System.out.println("State: " + state);
+			System.out.println();
+		}
 	}
 	
 	/**
@@ -597,20 +601,142 @@ public class Dungeone extends Canvas{
 	 * Creates an image to be displayed later
 	 */
 	public void buffer(){
-		//System.out.println(state);
 		Graphics2D g = buff.createGraphics();
 		g.setBackground(Color.black);
 		g.clearRect(0, 0, getWidth(), getHeight());
+		
+		//Intro
 		if(state == -1){
-			if(map == null){
 			g.setColor(Color.white);
-			g.drawLine(0, 0, 100, 100);
-			//TD: add an actual image
+			g.drawLine(20, 20, 20, 100); //D
+			g.drawLine(20, 20, 60, 60);
+			g.drawLine(60, 60, 20, 100);
+			g.drawLine(80, 20, 80, 100); //U
+			g.drawLine(80, 100, 120, 100);
+			g.drawLine(120, 20, 120, 100);
+			g.drawLine(140, 20, 140, 100); //N
+			g.drawLine(140, 20, 180, 100);
+			g.drawLine(180, 20, 180, 100);
+			g.drawLine(200, 20, 240, 20); //G
+			g.drawLine(200, 20, 200, 100);
+			g.drawLine(200, 100, 240, 100);
+			g.drawLine(240, 60, 240, 100);
+			g.drawLine(260, 20, 260, 100); //E
+			g.drawLine(260, 20, 300, 20);
+			g.drawLine(260, 60, 300, 60);
+			g.drawLine(260, 100, 300, 100);
+			g.drawLine(320, 20, 320, 100); //O
+			g.drawLine(320, 20, 360, 20);
+			g.drawLine(320, 100, 360, 100);
+			g.drawLine(360, 20, 360, 100);
+			g.drawLine(380, 20, 380, 100); //N
+			g.drawLine(380, 20, 420, 100);
+			g.drawLine(420, 20, 420, 100);
+			g.drawLine(440, 20, 440, 100); //E
+			g.drawLine(440, 20, 480, 20);
+			g.drawLine(440, 60, 480, 60);
+			g.drawLine(440, 100, 480, 100);
+			
+			g.drawString("Dungeone is a Turn-Based PvP Dungeone Exploratione Game, designed and produced by Rydler Industries.", 20, 120);
+			g.drawString("Player 1, the Dungeonee, is leading a party of adventurers through a perilous dungeone to find a hidden objective.", 20, 132);
+			g.drawString("-The Adventurers are generally stronger and more resilient than monsters", 30, 144);
+			g.drawString("-The Adventurers can only see in a small range around themselves", 30, 156);
+			g.drawString("-A dead Adventurer is pretty darn dead, so don't die", 30, 168);
+			g.drawString("-The Adventurers win if any Adventurer reaches the objective", 30, 180);
+			g.drawString("Player 2, the Dungeoneer, is creating and commanding a mob of monsters to protect the objective.", 20, 192);
+			g.drawString("-The Monsters know the happenings of the entire dungeone", 30, 204);
+			g.drawString("-The Monsters are generally weaker, and rely on ambushes and numbers to win fights", 30, 216);
+			g.drawString("-A dead Monster can be replaced, so dying is okay", 30, 228);
+			g.drawString("-The Monsters win if there are no Adventurers left on the map", 30, 240);
+			
+			g.drawString("Dungeone is played in turns, and uses both mouse and keyboard controls.", 20, 288);
+			g.drawString("Most controls involve either a Selected ( S ) or Picked ( P ) tile in a 2D map.", 20, 300);
+			g.drawRect(236, 290, 12, 12);
+			g.setColor(Color.green);
+			g.drawRect(315, 290, 12, 12);
+			g.setColor(Color.white);
+			g.drawString("When possible, the picked tile will provide information about all its layers.", 20, 312);
+			
+			g.drawString("Mouse Controls:", 20, 336);
+			g.drawString("Left Click (M1) sets both S and P to the clicked tile.", 20, 348);
+			g.drawString("-Left Click on P will create a generic unit (0) at the tile", 30, 360);
+			g.drawString("Right Click (M2) will set S to the clicked tile, and if possible command a unit at P to act on S.", 20, 372);
+			g.drawString("-Right Click on an empty tile will move P", 30, 384);
+			g.drawString("-Right Click on another unit will attack it with P", 30, 396);
+			g.drawString("-Right Click on a door will open it - clicking again will move P", 30, 408);
+			g.drawString("Middle Click (M3) will end your turn/confirm your turn end.", 20, 420);
+			
+			g.drawString("Keyboard Controls:", 20, 444);
+			g.drawString("WASD or Arrows move S.", 20, 456);
+			g.drawString("Space will set P to S or remove it.", 20, 468);
+			g.drawString("Z will move a unit from P to S.", 20, 480);
+			g.drawString("X will make a unit at P attack another unit at S.", 20, 492);
+			g.drawString("C will create a generic unit (0) at S.", 20, 504);
+			g.drawString("1-2 will create a special unit (1-2) at P.", 20, 516);
+			g.drawString("P will end your turn/confirm your turn end.", 20, 528);
+			g.drawString("R will toggle sight/attack range indicators.", 20, 540);
+			
+			g.drawString("Any action involving a unit costs some number of Action Points (AP).", 20, 564);
+			g.drawString("-AP are increased at the start of each turn, and a turn ends when they are depleted", 30, 576);
+			g.drawString("-AP can also be stored up to a maximum of 15 for later rounds by ending your turn early", 30, 588);
+			
+			g.drawString("Creating units works differently for each player.", 20, 612);
+			g.drawString("-Adventurers can only be created on the first turn, and must be adjacent to the spawn tile", 30, 624);
+			g.drawString("-Monsters cannot be created until the objective is placed: The first create will place the objective", 30, 636);
+
+			g.drawString("Tile Types:", 600, 252);
+			g.drawString("Walls - Can't go through 'em. Simple enough.", 600, 276);
+			g.drawString("Floors - You walk on 'em. Simple enough.", 600, 288);
+			g.drawString("Doors - Attack them to open or close them. Closed doors block movement and vision.", 600, 300);
+			g.setColor(Color.darkGray);
+			g.fillRect(580, 264, 12, 12);
+			g.setColor(new Color(110, 63, 25));
+			g.fillRect(580, 288, 12, 12);
+			g.setColor(new Color(176, 128, 96));
+			g.fillRect(568, 288, 12, 12);
+			g.setColor(Color.white);
+			
+			g.drawString("Objective - Goal of this game. Find it or protect it.", 600, 324);
+			g.drawString("Spawn - Start point for the adventurers. Also a source of vision.", 600, 336);
+			g.drawString("Teleporters - Connect disconnected tunnels. Can move and attack through, but vision is limited.", 600, 348);
+			g.setColor(new Color(0, 100, 0));
+			g.fillRect(580, 312, 12, 12);
+			g.setColor(new Color(110, 139, 61));
+			g.fillRect(580, 324, 12, 12);
+			g.setColor(new Color(176, 128, 96));
+			g.fillRect(580, 336, 12, 12);
+			g.setColor(Color.white);
+
+			
+			g.drawString("Fighter (0) - Basic adventurer. Hits things and tries not to die.", 600, 372);
+			g.drawString("Ranger (1) - Weaker than a Fighter, but can attack from a distance.", 600, 384);
+			g.drawString("Healer (2) - Cannot attack. Instead, attacks restore health to targets.", 600, 396);
+			g.setColor(new Color(0, 154, 205));
+			g.fillRect(580, 360, 12, 12);
+			g.setColor(new Color(66, 66, 111));
+			g.fillRect(580, 372, 12, 12);
+			g.setColor(new Color(95, 159, 159));
+			g.fillRect(580, 384, 12, 12);
+			g.setColor(Color.white);
+			
+			g.drawString("Slim (0) - Basic monster. Hits things but usually dies.", 600, 420);
+			g.drawString("Wall (1) - Immobile monster which cannot attack, used for temporarily blocking areas.", 600, 432);
+			g.drawString("Bulk (2) - Slow but powerful monster. Expensive but deadly.", 600, 444);
+			g.setColor(new Color(238, 44, 44));
+			g.fillRect(580, 408, 12, 12);
+			g.setColor(new Color(139, 71, 93));
+			g.fillRect(580, 420, 12, 12);
+			g.setColor(new Color(192, 0, 0));
+			g.fillRect(580, 432, 12, 12);
+			g.setColor(Color.white);
+
+			
+			if(map == null){
+				g.setColor(Color.red);
+				g.drawString("Map generation in progress, please wait.", 600, 540);
 			}
 			if(map!= null){
-				g.setColor(Color.white);
-				g.drawLine(0, 100, 100, 0);
-				//TD: add another actual image
+				g.drawString("Click or press any key to begin.", 600, 540);
 			}
 		}
 		else{
@@ -630,69 +756,79 @@ public class Dungeone extends Canvas{
 					}
 				}
 				
-				if(state == 0){ //spawn grants vision during setup
-					g.setColor(Color.black);
-//					SpawnTile spawn= null;
-//					for(int m = 0; m < WIDTH; m++)
-//						for(int n = 0; n < HEIGHT; n++)
-//							if(map.getTile(m, n, 1).tileType == Tile.SPAWN_TILE)
-//								spawn = (SpawnTile) map.getTile(m, n, 1);
-//					if(spawn != null && spawn.canSee(i, j)){
-					if(vision(i, j)){
-						if(map.getTile(i, j, 2).tileType >= Tile.ADVENTURER)
-							g.setColor(Color.blue);
-						else if(map.getTile(i, j, 2).tileType == Tile.WALL_TILE)
-							g.setColor(Color.darkGray);
-						else if(map.getTile(i, j, 1).tileType == Tile.DOOR_TILE){
-							if(((DoorTile) map.getTile(i, j, 1)).isOpen()) //change if open
-								g.setColor(new Color(176, 128, 96)); //light brown
-							else
-								g.setColor(new Color(110, 63, 25)); //dark brown
-						}
-						else if(map.getTile(i, j, 1).tileType == Tile.SPAWN_TILE)
-							g.setColor(Color.cyan);
-						else if (map.getTile(i, j, 1).tileType == Tile.WARP_TILE)
-							g.setColor(new Color(25, 25, 100));
-						else if(map.getTile(i, j, 0).tileType == Tile.FLOOR_TILE)
-							g.setColor(Color.lightGray);
-						else
-							g.setColor(Color.black);
-						g.fillRect(i*15+102, j*15+43, 10, 10);
-					}
-				}
+//				if(state == 0){ //spawn grants vision during setup
+//					g.setColor(Color.black);
+////					SpawnTile spawn= null;
+////					for(int m = 0; m < WIDTH; m++)
+////						for(int n = 0; n < HEIGHT; n++)
+////							if(map.getTile(m, n, 1).tileType == Tile.SPAWN_TILE)
+////								spawn = (SpawnTile) map.getTile(m, n, 1);
+////					if(spawn != null && spawn.canSee(i, j)){
+//					if(vision(i, j)){
+//						if(map.getTile(i, j, 2).tileType >= Tile.ADVENTURER)
+//							g.setColor(Color.blue);
+//						else if(map.getTile(i, j, 2).tileType == Tile.WALL_TILE)
+//							g.setColor(Color.darkGray);
+//						else if(map.getTile(i, j, 1).tileType == Tile.DOOR_TILE){
+//							if(((DoorTile) map.getTile(i, j, 1)).isOpen()) //change if open
+//								g.setColor(new Color(176, 128, 96)); //light brown
+//							else
+//								g.setColor(new Color(110, 63, 25)); //dark brown
+//						}
+//						else if(map.getTile(i, j, 1).tileType == Tile.SPAWN_TILE)
+//							g.setColor(Color.cyan);
+//						else if (map.getTile(i, j, 1).tileType == Tile.WARP_TILE)
+//							g.setColor(new Color(25, 25, 100));
+//						else if(map.getTile(i, j, 0).tileType == Tile.FLOOR_TILE)
+//							g.setColor(Color.lightGray);
+//						else
+//							g.setColor(Color.black);
+//						g.fillRect(i*15+102, j*15+43, 10, 10);
+//					}
+//				}
 				
-				if(state == 1){ //dungeonee sees by units and spawn, dungeoneer sees all
+				if(state == 0 || state == 1){ //dungeonee sees by units and spawn, dungeoneer sees all
 					switch(map.getTile(i, j, 2).tileType){
 						case Tile.WALL_TILE:
 							g.setColor(Color.darkGray);
 							break;
 						case Tile.FIGHTER:
+							g.setColor(new Color(0, 154, 205)); //deep sky blue
+							break;
 						case Tile.RANGER:
+							g.setColor(new Color(66, 66, 111)); //cornflower blue
+							break;
 						case Tile.HEALER:
-							g.setColor(Color.blue);
+							g.setColor(new Color(95, 159, 159)); //cadet blue
 							break;
 						case Tile.SLIM:
+							g.setColor(new Color(238, 44, 44)); //brick red
+							break;
 						case Tile.IMP:
+							//no color no imps
+							break;
 						case Tile.LIVING_WALL:
+							g.setColor(new Color(139, 71, 93)); //violet red
+							break;
 						case Tile.BULK:
-							g.setColor(Color.red);
+							g.setColor(new Color(192, 0, 0)); //free speech red
 							break;
 						default:	
 							switch(map.getTile(i, j, 1).tileType){
 								case Tile.OBJECTIVE:
-									g.setColor(Color.green);
+									g.setColor(new Color(0, 100, 0)); //dark green
 									break;
 								case Tile.SPAWN_TILE:
-									g.setColor(Color.cyan);
+									g.setColor(new Color(110, 139, 61)); //dark olive green
+									break;
+								case Tile.WARP_TILE:
+									g.setColor(new Color(34, 139, 34)); //forest green
 									break;
 								case Tile.DOOR_TILE:
 									if(((DoorTile) map.getTile(i, j, 1)).isOpen()) //change if open
 										g.setColor(new Color(176, 128, 96)); //light brown
 									else
 										g.setColor(new Color(110, 63, 25)); //dark brown
-									break;
-								case Tile.WARP_TILE:
-									g.setColor(new Color(25, 25, 100)); //dark blue
 									break;
 								default:
 									switch(map.getTile(i, j, 0).tileType){
@@ -715,7 +851,6 @@ public class Dungeone extends Canvas{
 		}
 		
 		//Ranges
-		//still needs work
 		if(ranges){
 		if (state == 0 || state == 1){
 			if(turn == 1){
@@ -731,10 +866,10 @@ public class Dungeone extends Canvas{
 					Actor a = (Actor) map.getTile(pick[0], pick[1], 2);
 					for(int i = 0; i < WIDTH; i++)
 						for(int j = 0; j < HEIGHT; j++){
-							if(a.canMoveTo(i, j)){
-								g.setColor(Color.green);
-								g.drawLine(i*15+100, j*15+40, i*15+100, j*15+47);
-							}
+//							if(a.canMoveTo(i, j)){
+//								g.setColor(Color.green);
+//								g.drawLine(i*15+100, j*15+40, i*15+100, j*15+47);
+//							}
 							if(a.canAttack(i, j)){
 								g.setColor(Color.red);
 								g.drawLine(i*15+100, j*15+48, i*15+100, j*15+55);
@@ -763,6 +898,7 @@ public class Dungeone extends Canvas{
 		g.drawString("X - Attack S w/ P", 0, 156);
 		g.drawString("C - Create at S", 0, 168);
 		g.drawString("P - Pass Turn", 0, 180);
+		g.drawString("R - Toggle Range", 0, 192);
 		
 		g.drawString("Mouse Controls:", 0, 204);
 		g.drawString("M1 - Set S and P", 0, 216);
@@ -833,63 +969,70 @@ public class Dungeone extends Canvas{
 					g.drawString("Fighter", 900, 180);
 					g.drawString("Health: " + temp.getCurrHP() + "/" + temp.getBaseHP(), 900, 192);
 					g.drawString("Attack: " + temp.getBaseAtt(), 900, 204);
-					g.drawString("Move: " + temp.getMoveRange(), 900, 216);
-					g.drawString("Adventurer with a sword.", 900, 228);
+					g.drawString("Range: " + temp.getAttRange(), 900, 216);
+					g.drawString("Move: " + temp.getMoveRange(), 900, 228);
+					g.drawString("Adventurer with a sword.", 900, 240);
 					break;
 				case Tile.RANGER:
 					temp = ((Actor)map.getTile(pick[0], pick[1], 2));
 					g.drawString("Ranger", 900, 180);
 					g.drawString("Health: " + temp.getCurrHP() + "/" + temp.getBaseHP(), 900, 192);
 					g.drawString("Attack: " + temp.getBaseAtt(), 900, 204);
-					g.drawString("Move: " + temp.getMoveRange(), 900, 216);
-					g.drawString("Adventurer with a bow.", 900, 228);
+					g.drawString("Range: " + temp.getAttRange(), 900, 216);
+					g.drawString("Move: " + temp.getMoveRange(), 900, 228);
+					g.drawString("Adventurer with a bow.", 900, 240);
 					break;
 				case Tile.HEALER:
 					temp = ((Actor)map.getTile(pick[0], pick[1], 2));
 					g.drawString("Healer", 900, 180);
 					g.drawString("Health: " + temp.getCurrHP() + "/" + temp.getBaseHP(), 900, 192);
 					g.drawString("Attack: " + temp.getBaseAtt(), 900, 204);
-					g.drawString("Move: " + temp.getMoveRange(), 900, 216);
-					g.drawString("Adventurer with a wand.", 900, 228);
+					g.drawString("Range: " + temp.getAttRange(), 900, 216);
+					g.drawString("Move: " + temp.getMoveRange(), 900, 228);
+					g.drawString("Adventurer with a wand.", 900, 240);
 					break;
 				case Tile.SLIM:
 					temp = ((Actor)map.getTile(pick[0], pick[1], 2));
 					g.drawString("Slim", 900, 180);
 					g.drawString("Health: " + temp.getCurrHP() + "/" + temp.getBaseHP(), 900, 192);
 					g.drawString("Attack: " + temp.getBaseAtt(), 900, 204);
-					g.drawString("Move: " + temp.getMoveRange(), 900, 216);
-					g.drawString("Kinda shady.", 900, 228);
+					g.drawString("Range: " + temp.getAttRange(), 900, 216);
+					g.drawString("Move: " + temp.getMoveRange(), 900, 228);
+					g.drawString("Kinda shady.", 900, 240);
 					break;
 				case Tile.IMP:
 					temp = ((Actor)map.getTile(pick[0], pick[1], 2));
 					g.drawString("Imp", 900, 180);
 					g.drawString("Health: " + temp.getCurrHP() + "/" + temp.getBaseHP(), 900, 192);
 					g.drawString("Attack: " + temp.getBaseAtt(), 900, 204);
-					g.drawString("Move: " + temp.getMoveRange(), 900, 216);
-					g.drawString("Kinda portable.", 900, 228);
+					g.drawString("Range: " + temp.getAttRange(), 900, 216);
+					g.drawString("Move: " + temp.getMoveRange(), 900, 228);
+					g.drawString("Kinda deported.", 900, 240);
 					break;
 				case Tile.LIVING_WALL:
 					temp = ((Actor)map.getTile(pick[0], pick[1], 2));
 					g.drawString("Wall", 900, 180);
 					g.drawString("Health: " + temp.getCurrHP() + "/" + temp.getBaseHP(), 900, 192);
 					g.drawString("Attack: " + temp.getBaseAtt(), 900, 204);
-					g.drawString("Move: " + temp.getMoveRange(), 900, 216);
-					g.drawString("Kinda something.", 900, 228);
+					g.drawString("Range: " + temp.getAttRange(), 900, 216);
+					g.drawString("Move: " + temp.getMoveRange(), 900, 228);
+					g.drawString("Kinda stuck.", 900, 240);
 					break;
 				case Tile.BULK:
 					temp = ((Actor)map.getTile(pick[0], pick[1], 2));
 					g.drawString("Bulk", 900, 180);
 					g.drawString("Health: " + temp.getCurrHP() + "/" + temp.getBaseHP(), 900, 192);
 					g.drawString("Attack: " + temp.getBaseAtt(), 900, 204);
-					g.drawString("Move: " + temp.getMoveRange(), 900, 216);
-					g.drawString("Kinda something else.", 900, 228);
+					g.drawString("Range: " + temp.getAttRange(), 900, 216);
+					g.drawString("Move: " + temp.getMoveRange(), 900, 228);
+					g.drawString("Kinda political.", 900, 240);
 					break;
 				default:
 					g.drawString("Nothing of interest", 900, 180);
 			}
 			}
 		if(!cont && !end)
-			g.drawString("Turn Ended. Hit R/P/M3 to Pass Turn.", 900, 300);
+			g.drawString("Turn Ended. Hit P/M3 to Pass Turn.", 900, 300);
 		}
 	}
 	}
